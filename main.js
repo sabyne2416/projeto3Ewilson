311119134cba91856616238d31d3117038d4e4c6
sabyne <105920674+sabyne2416@users.noreply.github.com>
2024-03-12 08:47:20 -0300

    a

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const botoes =  document.querySelectorAll (".botao");
 
-for( let i = 0 < botoes.length)
+for( let i = 0 < botoes.length; j++){
+    botoes[i];oneclick= function(){
+        for (let j = 0; j< botoes.length; j++){
+            botos[j].classlist.remove("ativo");
+        }
+        botoes[i].classlist.add("ativo")
+
+    }
+
+}
