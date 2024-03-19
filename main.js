0640c94be31ae7394c241fc0cf02c8433a5d5583
sabyne <105920674+sabyne2416@users.noreply.github.com>
2024-03-19 07:56:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes =  document.querySelectorAll (".botao");
 
-for( let i = 0 < botoes.length; j++){
+for( let i = 0; i < botoes.length; i++){
     botoes[i];oneclick= function(){
         for (let j = 0; j< botoes.length; j++){
             botos[j].classlist.remove("ativo");
