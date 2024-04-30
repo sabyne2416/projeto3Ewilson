037e9de3a1ae141570d2c8c754e8bc9adde27a30
sabyne <105920674+sabyne2416@users.noreply.github.com>
2024-04-30 07:54:33 -0300

    a

--- main.js
+++ main.js
@@ -14,7 +14,27 @@ for( let i = 0; i < botoes.length; i++){
 
 }
 const contadores = document.querySelectorAll("contador");
-const tempoObjetivo1 = new Date("2024-12-28T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
 let tempoAtual = new Date ();
+for (let i=0; i < contadores.length; i++){
+    contadores [i].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1)
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = new date ();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor( tempoFinal / 1000);
+    let minutos = MAth.floor (minutos / 60);
+    let horas = Math.floor( minutos / 60);
+    let dias = Math.floor (horas / 24);
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias"
+    + horas + "horas"
+    + minutos + " minutos"
+    + segundos + "segundos";
+
+}
